2857322ae98af657f17054ea3ff8e6288a79774c
gene-yoo <gene.y.yoo@gmail.com>
2017-10-30 12:57:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file
+function findMatching (drivers, name) {
+  return drivers.filter(function (n) { return n.toLowerCase() === name.toLowerCase(); });
+}
+
+function fuzzyMatch (drivers, string) {
+  return drivers.filter(function (n) { return n.toLowerCase().startsWith(string.toLowerCase()) })
+}
+
+function matchName (drivers, string) {
+  return drivers.filter(function (n) { return n.name === string })
+}
